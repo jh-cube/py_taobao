736effa908ddc5e5f26ee7a1c502ac362acb8ee7
Cube <437081444@qq.com>
2017-04-15 16:41:06 +0800

    Update taobao.py

--- taobao.py
+++ taobao.py
@@ -8,7 +8,6 @@ import re
 from selenium import webdriver
 from selenium.common.exceptions import TimeoutException
 from selenium.common.exceptions import StaleElementReferenceException
-
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
